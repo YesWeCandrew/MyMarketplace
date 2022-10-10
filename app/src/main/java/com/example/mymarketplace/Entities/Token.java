9a10b35af01d303d192fc74a29d72a1e6c341cdc
Matthew Cawley <u7313113@anu.edu.au>
2022-10-10 17:15:02 +1100

    added tokenizer

---
+++ app/src/main/java/com/example/mymarketplace/Entities/Token.java
@@ -0,0 +1,51 @@
+package com.example.mymarketplace.Entities;
+
+import java.util.Objects;
+public class Token {
+    // The following enum defines different types of tokens. Example of accessing these: Token.Type.INT
+    public enum Type {PNAME, SNAME, CAT, SUBCAT, PRICEMAX, PRICEMIN}
+
+    /**
+     * The following exception should be thrown if a tokenizer attempts to tokenize something that is not of one
+     * of the types of tokens.
+     */
+    public static class IllegalTokenException extends IllegalArgumentException {
+        public IllegalTokenException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    // Fields of the class Token.
+    private final String token; // Token representation in String form.
+    private final Type type;    // Type of the token.
+
+    public Token(String token, Type type) {
+        this.token = token;
+        this.type = type;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true; // Same hashcode.
+        if (!(other instanceof Token)) return false; // Null or not the same type.
+        return this.type == ((Token) other).getType() && this.token.equals(((Token) other).getToken()); // Values are the same.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(token, type);
+    }
+}
