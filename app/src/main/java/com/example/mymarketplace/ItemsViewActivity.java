4694800ad04f51baee97292f53a075759e3dec1c
Long Vu <u6699146@anu.edu.au>
2022-10-18 17:39:43 +1100

    Update listview with images,productName,sellerName,averagerating of each product + updated UI for ItemInfo

--- app/src/main/java/com/example/mymarketplace/ItemsViewActivity.java
+++ app/src/main/java/com/example/mymarketplace/ItemsViewActivity.java
@@ -1,12 +1,18 @@
 package com.example.mymarketplace;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -23,6 +29,12 @@ import com.example.mymarketplace.Entities.Users;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ItemsViewActivity extends AppCompatActivity {
 
@@ -33,6 +45,15 @@ public class ItemsViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_view);
+
+
+        // Change Colour of Action Bar & Status Bar
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.ocean)));
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.ocean));
+
 
         Users.User user = getIntent().getSerializableExtra("user", Users.User.class);
 
@@ -80,25 +101,45 @@ public class ItemsViewActivity extends AppCompatActivity {
 
 
         /**
-         * ListView of all the product names
+         * Displaying Products in ListView
          */
-        ListView itemListView = (ListView) findViewById(R.id.itemsListView);
-        swiperefresh = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
 
-        ArrayList<Items.Item> itemList = Items.getItems();
-        ArrayList<String> productName = new ArrayList<>();
+        ArrayList<Items.Item> itemList = Items.getItems(); // List of Items
+
+        int[] images = {R.drawable.item0, R.drawable.item1, R.drawable.item2, R.drawable.item3, R.drawable.item4, R.drawable.item5,
+                R.drawable.item6, R.drawable.item7, R.drawable.item8, R.drawable.item9, R.drawable.item10, R.drawable.item11,
+                R.drawable.item12, R.drawable.item13,R.drawable.item14, R.drawable.item15, R.drawable.item16, R.drawable.item17,
+                R.drawable.item18, R.drawable.item19, R.drawable.item20, R.drawable.item21, R.drawable.item22, R.drawable.item23,
+                R.drawable.item24, R.drawable.item25, R.drawable.item26, R.drawable.item27, R.drawable.item28, R.drawable.item29,
+                R.drawable.item30, R.drawable.item31, R.drawable.item32, R.drawable.item33, R.drawable.item34, R.drawable.item35,
+                R.drawable.item36, R.drawable.item37, R.drawable.item38, R.drawable.item39, R.drawable.item40, R.drawable.item41,
+                R.drawable.item42, R.drawable.item43, R.drawable.item44, R.drawable.item45, R.drawable.item46, R.drawable.item47,
+                R.drawable.item48, R.drawable.item49};
+
+        ArrayList<String> itemName = new ArrayList<>();
+        ArrayList<String> sellerName = new ArrayList<>();
+        ArrayList<String> averageRating = new ArrayList<>();
 
         for(int i=0; i<itemList.size(); i++) {
             Items.Item item = Items.getItems().get(i);
-            String itemVal = item.productName;
-            productName.add(itemVal);
+
+            String itemName2 = item.productName;
+            String sellerName2 = item.sellerName;
+            String averageRating2 = item.averageRatingAsText;
+
+            itemName.add(itemName2);
+            sellerName.add(sellerName2);
+            averageRating.add(averageRating2);
         }
 
-        myListAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, productName);
+        ListView itemListView = (ListView) findViewById(R.id.itemsListView);
 
-        itemListView.setAdapter(myListAdapter);
-        itemListView.setDivider(null);
-        itemListView.setClickable(true);
+        CustomListViewAdapter customListViewAdapter = new CustomListViewAdapter(this, images, itemName, sellerName, averageRating);
+        itemListView.setAdapter(customListViewAdapter);
+
+        // Swipe Refresh
+        swiperefresh = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+
         itemListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
